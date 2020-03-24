4a2282c6b3a03a87420e715cf75ecbb1fc349d7a
rstyczynski <rstyczynski@gmail.com>
2020-03-24 07:47:04 +0100

    .

--- setup.sh
+++ setup.sh
@@ -4,23 +4,27 @@ pmaker_home=/opt/pmaker
 
 cd setup
 ansible-playbook pmaker_create.yml
-if [ $? -eq 0 ]; then
-
-  sudo chown -R pmaker $pmaker_home
-
-  sudo su pmaker bash -c"
-  mkdir -p $pmaker_home/state
-  mkdir -p $pmaker_home/state/dev
-  mkdir -p $pmaker_home/state/sit
-  mkdir -p $pmaker_home/state/uat
-  mkdir -p $pmaker_home/state/prod
-  
-  cd
-  git clone https://github.com/rstyczynski/pmaker.git
-  cp -r pmaker/* $pmaker_home/
-  "
-
-  exit 0
+if [ $? -ne 0 ]; then
+  echo "Error. Installation error. Procedure broken."
+  exit 1
 fi
 
-exit 1
+sudo chown -R pmaker $pmaker_home
+
+sudo su pmaker bash -c"
+mkdir -p $pmaker_home/state
+mkdir -p $pmaker_home/state/dev
+mkdir -p $pmaker_home/state/sit
+mkdir -p $pmaker_home/state/uat
+mkdir -p $pmaker_home/state/prod
+
+cd
+git clone https://github.com/rstyczynski/pmaker.git
+cp -r pmaker/* $pmaker_home/
+
+cd /opt/pmaker
+\rm -f setup.sh
+\rm -rf setup
+"
+
+exit 0
