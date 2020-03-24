8e734debec521eb3ea739ce7f9cdb308607986fd
rstyczynski <rstyczynski@gmail.com>
2020-03-24 07:32:51 +0100

    .

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ cd setup
 ansible-playbook pmaker_create.yml
 if [ $? -eq 0 ]; then
   cd
-  mkdir state
-  mkdir state/dev
-  mkdir state/sit
-  mkdir state/uat
-  mkdir state/prod
+  mkdir -p state
+  mkdir -p state/dev
+  mkdir -p state/sit
+  mkdir -p state/uat
+  mkdir -p state/prod
   exit 0
 fi
 
