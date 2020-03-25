98010bd067809db5369504278164ec607d831006
rstyczynski <rstyczynski@gmail.com>
2020-03-25 21:15:25 +0100

    .

--- lib/generate_welcome_msg.sh
+++ lib/generate_welcome_msg.sh
@@ -35,7 +35,7 @@ function getUserData() {
 	export key_ssh_enc=$(cat state/$user_group/$server_group/$username/.ssh/id_rsa.enc)
 	export key_ppk_enc=$(cat state/$user_group/$server_group/$username/.ssh/id_rsa.ppk)
 
-	export jump_server=$(cat data/$user_group.inventory.cfg | grep -A1 $server_group\_jump | grep ansible_user | cut -f1 -d' ')
+	export jump_server=$(cat data/$user_group.inventory.cfg | grep -A1 $server_group\_jump | grep public_ip | tr ' ' '\n' | grep public_ip | cut -f2 -d=)
 	export date=$(date +"%F %T")
 
 	if [ -z "$admin" ]; then
