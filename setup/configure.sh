bde279fb98f3d9840ddf1acc04662567d01922e0
rstyczynski <rstyczynski@gmail.com>
2020-03-25 21:22:26 +0100

    explit setup & configure

---
+++ setup/configure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+cat data/sample.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" > setup/inventory.cfg
+
+ansible -m ping all -i setup/inventory.cfg
+if [ $? -ne 0 ]; then
+  echo "Error. SSH communication not possible to all servers. Fix the erros and retry. Exiting."
+  exit 1
+fi
+
+ansible-playbook controller -i setup/inventory.cfg setup/pmaker_create.yaml
+if [ $? -ne 0 ]; then
+  echo "Error. Installation error. Procedure broken. Fix the erros and retry. Exiting."
+  exit 2
+fi
+
--- setup/setup.sh
+++ setup/setup.sh
@@ -14,15 +14,8 @@ else
   git pull
 fi
 
-cat data/sample.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" > setup/inventory.cfg
 
-ansible -m ping all -i setup/inventory.cfg
-if [ $? -ne 0 ]; then
-  echo "Error. SSH communication not possible to all servers. Fix the erros and retry. Exiting."
-  exit 1
-fi
-
-ansible-playbook -i setup/inventory.cfg setup/pmaker_create.yaml
+ansible-playbook controller -i setup/controller.inventory.cfg setup/pmaker_create.yaml
 if [ $? -ne 0 ]; then
   echo "Error. Installation error. Procedure broken. Fix the erros and retry. Exiting."
   exit 2
