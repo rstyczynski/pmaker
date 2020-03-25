6314aecc707cc212a48f060a3a0fe95c16cffb30
rstyczynski <rstyczynski@gmail.com>
2020-03-26 00:24:37 +0100

    create workers fix

--- setup/configure.sh
+++ setup/configure.sh
@@ -4,6 +4,7 @@ if [ ! -f setup/inventory.cfg ]; then
   cat data/sample.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" >setup/inventory.cfg
 fi
 
+export NSIBLE_HOST_KEY_CHECKING=False
 ansible -m ping all -i setup/inventory.cfg
 if [ $? -ne 0 ]; then
   echo "Error. SSH communication not possible to all servers. Fix the erros and retry. Exiting."
