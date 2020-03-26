054c0f6587c4d3be493fe9befdb5fdcdf1a8ddea
rstyczynski <rstyczynski@gmail.com>
2020-03-26 16:20:30 +0100

    process subset of servers

--- envs_update.sh
+++ envs_update.sh
@@ -10,12 +10,8 @@ function y2j {
    ruby -ryaml -rjson -e 'puts JSON.dump(YAML.load(STDIN.read))'
 }
 
-server_groups=$(cat data/$user_group.users.yaml | 
-y2j | 
-jq -r '.users[].server_groups' | 
-grep '"' | 
-sed 's/[", ]//g' | 
-sort -u)
+server_groups=$(cat data/$user_group.users.yaml |  y2j |  jq -r '[.users[].server_groups[]] | unique | .[]')
+#Source: https://stackoverflow.com/questions/29822622/get-all-unique-json-key-names-with-jq
 
 echo '==========================================================================='
 echo Spliting usaers into group using: $server_groups
@@ -35,15 +31,19 @@ echo '==========================================================================
 echo Users ready.
 echo '==========================================================================='
 
+
 echo '==========================================================================='
 echo Now updating environments: $server_groups
 echo '==========================================================================='
 for server_group in $server_groups; do
+   server_list=$(ansible-inventory -i data/$user_group.inventory.cfg  -y --list | y2j | jq -r  '[.all.children.dev.hosts | keys[]] | unique | .[]')
+
    echo '========================='
    echo Processing env: $server_group
+   echo \-having servers: $server_list
    echo '========================='
 
-   ansible-playbook env_configure.yaml -e server_group=dev -e user_group=ocs -i data/ocs.inventory.cfg 
+   ansible-playbook env_configure.yaml -e server_group=$server_group -e user_group=$user_group -i data/$user_group.inventory.cfg -l "$server_list"
 
    echo '========================='
    echo Done.
