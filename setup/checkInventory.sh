d54acc31856e1305c39cc9f2ad07bfe9de6c9fbe
rstyczynski <rstyczynski@gmail.com>
2020-03-30 21:47:45 +0200

    .

---
+++ setup/checkInventory.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+server_group=$1; shift
+
+ansible -m ping all -i data/$server_group.inventory.cfg $@
+if [ $? -ne 0 ]; then
+  mkdir -p publicl chmod o+r+x public
+  cp data/$server_group.inventory.cfg public
+  chmod o+r public/*
+  echo Some of machnes not accesible for pmaker. 
+  echo Connect from different session as opc and prepare hosts for pmaker using setup/configureInventory.sh
+else
+  echo Good to go.
+fi
