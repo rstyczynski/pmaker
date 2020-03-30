f3106ca729e56170e5546c08e4a2788cec76c62c
rstyczynski <rstyczynski@gmail.com>
2020-03-30 21:21:23 +0200

    pass args to ansible

--- setup/configure.sh
+++ setup/configure.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+user_group=$1; shift
 
-if [ ! -f setup/inventory.cfg ]; then
-  cat data/sample.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" >setup/inventory.cfg
+if [ -z "$user_group" ]; then
+  user_group=sample
 fi
+cat data/$user_group.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" >setup/inventory.cfg
 
-export NSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 # hosts=$(cat setup/inventory.cfg | egrep '[0-9]+.' | grep -v '^#' | cut -f1 -d' ')
 # for host in $hosts; do
@@ -12,13 +14,13 @@ export NSIBLE_HOST_KEY_CHECKING=False
 # 	ssh-keyscan -H $host >>~/.ssh/known_hosts
 # done
 
-ansible -m ping all -i setup/inventory.cfg
+ansible -m ping all -i setup/inventory.cfg $@
 if [ $? -ne 0 ]; then
   echo "Error. SSH communication not possible to all servers. Fix the erros and retry. Exiting."
   exit 1
 fi
 
-ansible-playbook -i setup/inventory.cfg setup/pmaker_create.yaml
+ansible-playbook -i setup/inventory.cfg setup/pmaker_create.yaml $@
 if [ $? -ne 0 ]; then
   echo "Error. Installation error. Procedure broken. Fix the erros and retry. Exiting."
   exit 2
