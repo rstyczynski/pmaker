ac7c7fbc16dbe3242f4595536d3c4f0d2efcea34
rstyczynski <rstyczynski@gmail.com>
2020-03-30 22:45:05 +0200

    .cfg inv

--- setup/checkInventory.sh
+++ setup/checkInventory.sh
@@ -3,9 +3,9 @@ server_group=$1; shift
 
 ansible -m ping all -i data/$server_group.inventory.cfg $@
 if [ $? -ne 0 ]; then
-  mkdir -p publicl chmod o+r+x public
-  cp data/$server_group.inventory.cfg public
-  chmod o+r public/*
+  #mkdir -p publicl chmod o+r+x public
+  #cp data/$server_group.inventory.cfg public
+  #chmod o+r public/*
   echo Some of machnes not accesible for pmaker. 
   echo Connect from different session as opc and prepare hosts for pmaker using:
   echo setup/configureInventory.sh $server_group $@
--- setup/configureInventory.sh
+++ setup/configureInventory.sh
@@ -3,30 +3,30 @@ user_group=$1; shift
 
 if [ -z "$user_group" ]; then
   user_group=sample
-  cat data/$user_group.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" >setup/inventory.cfg
+  cat data/$user_group.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" >data/$user_group.inventory.opc
 else
-  cat /opt/pmaker/public/$user_group.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" >setup/inventory.cfg
+  cat data/$user_group.inventory.cfg | sed "s/=pmaker/=$(whoami)/g" >data/$user_group.inventory.opc
 fi
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-# hosts=$(cat setup/inventory.cfg | egrep '[0-9]+.' | grep -v '^#' | cut -f1 -d' ')
+# hosts=$(cat data/$user_group.inventory.cfg  | egrep '[0-9]+.' | grep -v '^#' | cut -f1 -d' ')
 # for host in $hosts; do
 # 	echo $host...
 # 	ssh-keyscan -H $host >>~/.ssh/known_hosts
 # done
 
-ansible -m ping all -i setup/inventory.cfg $@
+ansible -m ping all -i data/$user_group.inventory.opc $@
 if [ $? -ne 0 ]; then
   echo "Error. SSH communication not possible to all servers. Fix the erros and retry. Exiting."
   exit 1
 fi
 
-ansible-playbook -i setup/inventory.cfg setup/pmaker_create.yaml $@
+ansible-playbook -i data/$user_group.inventory.opc setup/pmaker_create.yaml $@
 if [ $? -ne 0 ]; then
   echo "Error. Installation error. Procedure broken. Fix the erros and retry. Exiting."
   exit 2
 fi
 
-rm -f setup/inventory.cfg
+rm -f data/$user_group.inventory.opc
 
