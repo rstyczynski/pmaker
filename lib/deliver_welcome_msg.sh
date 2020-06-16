219c6e26783ef398c64ce7750d8ccf03be419912
rstyczynski <rstyczynski@gmail.com>
2020-06-16 09:55:36 +0200

    .

--- lib/deliver_welcome_msg.sh
+++ lib/deliver_welcome_msg.sh
@@ -22,24 +22,30 @@ function welcome_email() {
                     TO_EMAIL_ADDRESS=$(getWelcomeEmail $user_group $env $user header | head -5 | grep 'TO:' | cut -d' ' -f2-999)
                     EMAIL_SUBJECT=$(getWelcomeEmail $user_group $env $user header | head -5 | grep 'SUBJECT:' | cut -d' ' -f2-999)
 
-                    getWelcomeEmail $user_group $env $user | 
-                    mailx -v -s "$EMAIL_SUBJECT" \
-                    -S nss-config-dir=/etc/pki/nssdb/ \
-                    -S smtp-use-starttls \
-                    -S ssl-verify=ignore \
-                    -S smtp=smtp://$SMTP_ADDRESS:$SMTP_PORT \
-                    -S from=$FROM_EMAIL_ADDRESS \
-                    -S smtp-auth-user=$ACCOUNT_USER \
-                    -S smtp-auth-password=$ACCOUNT_PASSWORD \
-                    -S smtp-auth=plain \
-                    $TO_EMAIL_ADDRESS
+                    if [ ! -z "$TO_EMAIL_ADDRESS" ]; then
+                        timoeut 30 getWelcomeEmail $user_group $env $user | 
+                        mailx -v -s "$EMAIL_SUBJECT" \
+                        -S nss-config-dir=/etc/pki/nssdb/ \
+                        -S smtp-use-starttls \
+                        -S ssl-verify=ignore \
+                        -S smtp=smtp://$SMTP_ADDRESS:$SMTP_PORT \
+                        -S from=$FROM_EMAIL_ADDRESS \
+                        -S smtp-auth-user=$ACCOUNT_USER \
+                        -S smtp-auth-password=$ACCOUNT_PASSWORD \
+                        -S smtp-auth=plain \
+                        $TO_EMAIL_ADDRESS 2> /tmp/email.$$.tmp
 
-                    if [ $? -eq 0 ]; then
-                        echo "Done."
-                        touch state/$user_group/$env/$user/welcome.sent
+                        if [ $? -eq 0 ]; then
+                            echo "Done."
+                            cat /tmp/email.$$.tmp > state/$user_group/$env/$user/welcome.sent
+                        else
+                            echo "Error sending email. Code: $?. Connect log: "
+                            cat /tmp/email.$$.tmp
+                            read -p "press any key"
+                        fi
+                        rm /tmp/email.$$.tmp
                     else
-                        echo "Error code:$?."
-                        read -p "press any key"
+                        echo Welcome mail not ready.
                     fi
                 else
                     echo 
@@ -72,16 +78,21 @@ function welcome_sms() {
     for user in $(cat data/$user_group.users.yaml | y2j | jq -r '.users[].username'); do
         mobile=$(cat data/$user_group.users.yaml | y2j | jq -r ".users[] | select(.username==\"$user\") | .mobile")
 
-        for env in $envs; do
-            echo -n ">>> $env $user: "
-            if [ ! -f state/$user_group/$env/$user/sms.sent ]; then
+        if [ ! -z "$mobile" ]; then
+            for env in $envs; do
+                echo -n ">>> $env $user: "
+                if [ ! -f state/$user_group/$env/$user/sms.sent ]; then
 
-                sms_message=$(getKeySMS $user_group $env $user)
-                aws sns publish --message "$sms_message" --phone-number "$mobile" | tee state/$user_group/$env/$user/sms.sent
-            else
-                echo "SMS already sent at $(ls -l state/$user_group/$env/$user/welcome.sent | cut -d' ' -f6-8)"
-            fi
-        done
+                    sms_message=$(getKeySMS $user_group $env $user)
+                    aws sns publish --message "$sms_message" --phone-number "$mobile" | tee state/$user_group/$env/$user/sms.sent
+                else
+                    echo "SMS already sent at $(ls -l state/$user_group/$env/$user/welcome.sent | cut -d' ' -f6-8)"
+                fi
+            done
+        else
+            echo Welcome sms not ready.
+        fi
+
     done
 }
 
