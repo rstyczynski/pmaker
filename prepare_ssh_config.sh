a83d1e32ba84524b9d648359ebdfe1b0072bc8dc
rstyczynski <rstyczynski@gmail.com>
2020-06-17 15:03:30 +0200

    .

--- prepare_ssh_config.sh
+++ prepare_ssh_config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 user_group=$1
 server_group=$2
+proxy_user=$3
+server_group_key=$4
 
 function usage() {
     echo Usage: prepare_ssh_config.sh user_group server_group
@@ -8,20 +10,29 @@ function usage() {
 
 if [ -z "$user_group" ] || [ -z "$server_group" ]; then
     usage
+    exit 1
 fi
 
-server_groups=$(cat data/$user_group.inventory.cfg | grep '\[' | cut -f2 -d'[' | cut -f1 -d']' | grep -v jumps | grep -v controller)
+: ${proxy_user:=opc}
+: ${server_group_key:=yes}
 
+server_groups=$(cat data/$user_group.inventory.cfg | grep '\[' | cut -f2 -d'[' | cut -f1 -d']' | grep -v jumps | grep -v controller)
 jump_server=$(cat data/$user_group.inventory.cfg | sed -n "/\[$server_group\]/,/^\[/p" | grep -v '\[' | grep -v '^$' | grep 'host_type=jump' | tr -s ' ' | tr ' ' '\n' | grep public_ip | cut -d'=' -f2)
 
 cat >>~/.ssh/config <<EOF
 
 host ${server_group}_jump
     HostName $jump_server
-    User opc
-    IdentityFile ~/.ssh/${server_group}.key
     ForwardAgent yes
+    User $proxy_user
+    IdentityFile ~/.ssh/${server_group}.key
 EOF
+
+if [ $server_group_key == yes ]; then
+cat >>~/.ssh/config <<EOF
+    IdentityFile ~/.ssh/${server_group}.key
+EOF
+fi
 
 hosts=$(cat data/ocs.inventory.cfg | sed -n "/\[$server_group\]/,/^\[/p" | grep -v '\[' | grep -v '^$' | grep -v 'host_type=jump' | cut -f1 -d' ')
 
