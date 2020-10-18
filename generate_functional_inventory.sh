72aaa80c5ebb6185d0f323849e3e649f457e293b
rstyczynski <rstyczynski@gmail.com>
2020-10-18 13:46:04 +0200

    finctional inventory

---
+++ generate_functional_inventory.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#
+# functions
+#
+
+function prepare_functional_inventory() {
+    user_group=$1
+    inventory_file=$2
+
+    if [ -z "$user_group" ]; then
+        echo "Error. Group can't be empty."
+        echo
+        echo "Usage: prepare_functional_inventory group inventory"
+        return 1
+    fi
+
+    if [ ! -f $inventory_file ]; then
+        echo "Error. Inventory file does not exist"
+        echo
+        echo "Usage: prepare_functional_inventory group inventory"
+        return 1
+    fi
+
+    if [ ! -d state/$user_group ]; then
+        echo "Error. Group does not exist."
+        echo
+        echo "Usage: prepare_functional_inventory group inventory"
+        return 1
+    fi
+
+    #
+    # global
+    #
+    host_product_prev=''
+    mkdir -p state/$user_group/functional
+
+    if [ state/$user_group/functional/inventory.cfg -ot $inventory_file ]; then
+        rm -f state/$user_group/functional/inventory.cfg
+
+        host_products=$(cat $inventory_file | perl -ne '/host_product=(\w+) / && print($1 ,"\n")' | sort -u)
+        for host_product in $host_products; do
+            if [ "$host_product" != "$host_product_prev" ]; then
+                echo "[$host_product]" >>state/$user_group/functional/inventory.cfg
+            fi
+            grep "host_product=$host_product" $inventory_file >>state/$user_group/functional/inventory.cfg
+            echo "" >>state/$user_group/functional/inventory.cfg
+        done
+        echo "Global functional inventory file created."
+    else
+        echo "Global functional inventory up to date."
+    fi
+
+    #
+    # build per server group inventory in state dir
+    #
+
+    server_groups=$(cat data/ocs.inventory.cfg | grep '^\[' | egrep -v '\[jumps\]|\[controller\]' | tr -d '[\[\]]')
+
+    for server_group in $server_groups; do
+        cat data/ocs.inventory.cfg | sed -n "/\[$server_group\]/,/\[/p" | sed '$ d' > state/$user_group/$server_group/inventory.cfg
+    done
+
+    #
+    # buibuild per server group functional inventory
+    #
+
+    for server_group in $server_groups; do
+        host_product_prev=''
+        mkdir -p state/$user_group/$server_group/functional
+
+        if [ state/$user_group/$server_group/functional/inventory.cfg -ot state/$user_group/$server_group/inventory.cfg ]; then
+            rm -f state/$user_group/$server_group/functional/inventory.cfg
+
+            host_products=$(cat state/$user_group/$server_group/inventory.cfg | perl -ne '/host_product=(\w+) / && print($1 ,"\n")' | sort -u)
+
+            for host_product in $host_products; do
+                if [ "$host_product" != "$host_product_prev" ]; then
+                    echo "[$host_product]" | tee -a state/$user_group/$server_group/functional/inventory.cfg
+                fi
+                grep "host_product=$host_product" state/$user_group/$server_group/inventory.cfg | tee -a state/$user_group/$server_group/functional/inventory.cfg
+                echo "" | tee -a  state/$user_group/$server_group/functional/inventory.cfg
+            done
+            echo "Server group functional inventory file created for $server_group."
+        else
+            echo "Server group functional inventory up to date for $server_group."
+        fi   
+    done 
+
+}
---
+++ pmaker_ssh_config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+user_group=$1
+
+if [ -z "$user_group " ]; then
+    echo "Usage: pmaker_ssh_config user_group"
+    echo
+    echo "Warning: script replace .ssh/config. Backup copy is made."
+    exit 1
+fi
+
+if [ ! -f data/$user_group.inventory.cfg ]; then
+    echo "Error: data/$user_group.inventory.cfg doe not exist!"
+    exit 2
+fi
+
+# 
+# build ssh config
+#
+
+cd state/$user_group
+server_groups=$(ls -d */ | egrep -v 'functional|outbox' | tr -d '/')
+cd - 2>/dev/null
+
+if [ -f ~/.ssh/config ]; then
+  mv ~/.ssh/config ~/.ssh/config_$(date -I).bak
+fi 
+
+for server_group in $server_groups; do
+   if [ -f state/$user_group/$server_group/pmaker/.ssh/id_rsa ]; then
+      echo "Settgn up ssh config for $server_group"
+      $pmaker_home/prepare_ssh_config.sh $user_group $server_group pmaker state/$user_group/$server_group/pmaker/.ssh/id_rsa
+   else
+     echo No pmaker key for $server_group. Using global pmaker key
+     $pmaker_home/prepare_ssh_config.sh $user_group $server_group pmaker ~/.ssh/id_rsa
+   fi
+done
