9475ee8b23f5aded85776e266cf1a56926fcd8e3
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2022-01-26 07:43:51 +0100

    functional inventory by variable

--- bin/generate_functional_inventory.sh
+++ bin/generate_functional_inventory.sh
@@ -4,88 +4,80 @@
 # functions
 #
 
+# generate server's group inventory file by given variable e.g. host_product to have access to all WLS servers in the environment 
+function build_functional_inventory() {
+    group_by_variable=$1
+
+    for server_group in $server_groups; do
+        mkdir -p $pmaker_home/state/$user_group/$server_group/functional
+
+        if [ $pmaker_home/state/$user_group/$server_group/functional/$group_by_variable.cfg -ot $pmaker_home/state/$user_group/$server_group/inventory.cfg ]; then
+            rm -f $pmaker_home/state/$user_group/$server_group/functional/$group_by_variable.cfg
+
+            keys=$(cat $pmaker_home/state/$user_group/$server_group/inventory.cfg | perl -ne "/$group_by_variable=(\w+) / && print(\$1 ,\"\n\")" 2>/dev/null | sort -u)
+
+            for group_by in $keys; do
+                echo "[$group_by]" | tee -a $pmaker_home/state/$user_group/$server_group/functional/$group_by_variable.cfg
+
+                grep "$group_by_variable=$group_by" $pmaker_home/state/$user_group/$server_group/inventory.cfg | tee -a $pmaker_home/state/$user_group/$server_group/functional/$group_by_variable.cfg
+                echo "" | tee -a $pmaker_home/state/$user_group/$server_group/functional/$group_by_variable.cfg
+
+                group_by_prev=$group_by
+            done
+            echo "Server group functional inventory by $group_by_variable file created for $server_group."
+            # backward compatiblity
+            if [ $group_by_variable == 'host_product' ]; then 
+                cp $pmaker_home/state/$user_group/$server_group/functional/$group_by_variable.cfg $pmaker_home/state/$user_group/$server_group/functional/inventory.cfg
+            fi
+        else
+            echo "Server group functional inventory by $group_by_variable up to date for $server_group."
+        fi   
+    done 
+}
+
 function generate_functional_inventory() {
     user_group=$1
     inventory_file=$2
 
     if [ -z "$user_group" ]; then
-        echo "Error. Group can't be empty."
+        echo "Error. User group can't be empty."
         echo
-        echo "Usage: prepare_functional_inventory group inventory"
+        echo "Usage: prepare_functional_inventory user_group inventory_file"
         return 1
     fi
 
     if [ ! -f $inventory_file ]; then
         echo "Error. Inventory file does not exist"
         echo
-        echo "Usage: prepare_functional_inventory group inventory"
+        echo "Usage: prepare_functional_inventory user_group inventory_file"
         return 1
     fi
 
     if [ ! -d $pmaker_home/state/$user_group ]; then
-        echo "Error. Group does not exist."
+        echo "Error. User group does not exist."
         echo
-        echo "Usage: prepare_functional_inventory group inventory"
+        echo "Usage: prepare_functional_inventory user_group inventory_file"
         return 1
-    fi
-
-    #
-    # global
-    #
-    host_product_prev=''
-    mkdir -p $pmaker_home/state/$user_group/functional
-
-    if [ $pmaker_home/state/$user_group/functional/inventory.cfg -ot $inventory_file ]; then
-        rm -f $pmaker_home/state/$user_group/functional/inventory.cfg
-
-        host_products=$(cat $inventory_file | perl -ne '/host_product=(\w+) / && print($1 ,"\n")' 2>/dev/null | sort -u)
-        for host_product in $host_products; do
-            if [ "$host_product" != "$host_product_prev" ]; then
-                echo "[$host_product]" >> $pmaker_home/state/$user_group/functional/inventory.cfg
-            fi
-            grep "host_product=$host_product" $inventory_file >> $pmaker_home/state/$user_group/functional/inventory.cfg
-            echo "" >> $pmaker_home/state/$user_group/functional/inventory.cfg
-        done
-        echo "Global functional inventory file created."
-    else
-        echo "Global functional inventory up to date."
     fi
 
     #
     # build per server group inventory in state dir
     #
-
-    server_groups=$(cat $pmaker_home/data/ocs.inventory.cfg | grep '^\[' | egrep -v '\[jumps\]|\[controller\]' | tr -d '[\[\]]')
+    server_groups=$(cat $pmaker_home/data/$user_group.inventory.cfg | grep '^\[' | egrep -v '\[jumps\]|\[controller\]' | tr -d '[\[\]]')
 
     for server_group in $server_groups; do
-        cat $pmaker_home/data/ocs.inventory.cfg | sed -n "/\[$server_group\]/,/\[/p" | sed '$ d' > $pmaker_home/state/$user_group/$server_group/inventory.cfg
+        cat $pmaker_home/data/$user_group.inventory.cfg | sed -n "/\[$server_group\]/,/\[/p" | sed '$ d' > $pmaker_home/state/$user_group/$server_group/inventory.cfg
     done
 
     #
-    # buibuild per server group functional inventory
+    # build product inventory
     #
+    build_functional_inventory host_product
 
-    for server_group in $server_groups; do
-        host_product_prev=''
-        mkdir -p $pmaker_home/state/$user_group/$server_group/functional
-
-        if [ $pmaker_home/state/$user_group/$server_group/functional/inventory.cfg -ot $pmaker_home/state/$user_group/$server_group/inventory.cfg ]; then
-            rm -f $pmaker_home/state/$user_group/$server_group/functional/inventory.cfg
-
-            host_products=$(cat $pmaker_home/state/$user_group/$server_group/inventory.cfg | perl -ne '/host_product=(\w+) / && print($1 ,"\n")' 2>/dev/null | sort -u)
-
-            for host_product in $host_products; do
-                if [ "$host_product" != "$host_product_prev" ]; then
-                    echo "[$host_product]" | tee -a $pmaker_home/state/$user_group/$server_group/functional/inventory.cfg
-                fi
-                grep "host_product=$host_product" $pmaker_home/state/$user_group/$server_group/inventory.cfg | tee -a $pmaker_home/state/$user_group/$server_group/functional/inventory.cfg
-                echo "" | tee -a $pmaker_home/state/$user_group/$server_group/functional/inventory.cfg
-            done
-            echo "Server group functional inventory file created for $server_group."
-        else
-            echo "Server group functional inventory up to date for $server_group."
-        fi   
-    done 
+    #
+    # build component inventory
+    #
+    build_functional_inventory host_component
 
 }
 
